ee5930edca98f44b3b482940de92c6e97031fcd2
rcn8263 <rcn8263@rit.edu>
2023-09-27 15:29:08 -0400

    fixed bug when searching with special chars

--- Search.py
+++ Search.py
@@ -2,7 +2,6 @@ import sqlite3
 import pandas as pd
 import streamlit as st
 
-
 st.set_page_config(
     page_title="SSSniperWolf-DB"
 )
@@ -15,14 +14,26 @@ con = sqlite3.connect("wolf.db")
 cur = con.cursor()
 
 if search:
+    # remove special characters from string. must follow these rules:
+    # Non-ASCII range characters (i.e. unicode codepoints greater than 127), or
+    # One of the 52 upper and lower case ASCII characters, or
+    # One of the 10 decimal digit ASCII characters, or
+    # The underscore character (unicode codepoint 96).
+    # The substitute character (unicode codepoint 26).
+    filtered_search = ''.join(letter for letter in search if letter.isalnum() or
+                      letter == 96 or letter == 26)
+
+    search_data = (filtered_search,)
     rows = cur.execute("SELECT id, title, upload_date, timestamp, transcript "
                        "FROM (SELECT * FROM transcripts WHERE transcript "
                        "MATCH ?) INNER JOIN videos ON "
                        "video_id = videos.id",
-                       [search]).fetchall()
+                       search_data).fetchall()
 
     df = pd.DataFrame(rows, columns=["Video ID", "Video Title", "Upload Date",
                                      "Timestamp (Minutes)", "Transcript"])
+    df = df.sort_values(["Upload Date", "Timestamp (Minutes)"],
+                        ascending=[False, True]).reset_index()
 
     st.write('Found ' + str(len(df)) + ' occurrences.')
 
